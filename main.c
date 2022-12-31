43f7383022cff1af34e232d07235c3abb9784dac
Alireza <mohaghegh.ar82@gmail.com>
2022-12-31 16:54:07 +0330

    cat

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 #define ll long long int
 #define MAX_VAL 10000
 
-void files(char path[]){
+void files(char path[])
+{
     int k = 0;
     char filename[MAX_VAL] = {}, dir[MAX_VAL] = {};
     for (ll i = 1 ; i < strlen(path) ; ++i){
@@ -17,10 +18,13 @@ void files(char path[]){
         while (i < j) dir[k++] = path[i++];
         if (i == strlen(path)){
             FILE *fp = NULL;
-            fp = fopen(dir, "a");
-            if (fp != NULL) printf("file is already existing\n");
-            fclose(fp);
-            return;
+            fp = fopen(dir, "rb");
+            if (fp != NULL) {printf("file is already existing\n"); fclose(fp); return;}
+            else{
+                fp = fopen(dir, "a");
+                fclose(fp);
+                return;
+            }
         }
         mkdir(dir, 0700);
         dir[k++] = '/';
@@ -48,6 +52,29 @@ void creatfile()
     // fclose(fp);
 }
 
+void cat()
+{
+    char option[100], filename[MAX_VAL];
+    scanf(" -%s", option);
+    scanf("%s", filename);
+    if (filename[0] == '/'){
+        for (ll i = 1 ; i < strlen(filename) ; i++){
+            filename[i - 1] = filename[i];
+        }
+        filename[strlen(filename) - 1] = '\0';
+    }
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (fp == NULL){printf("file not found\n"); return;}
+    int c;
+    while(1)
+   {
+      c = fgetc(fp);
+      if(feof(fp)) break;
+      printf("%c", c);
+   }
+}
+
 int main()
 {
     char command[100];
@@ -57,6 +84,7 @@ int main()
         scanf("%s", command);
         if (strcmp(command ,"exit") == 0) run = 0;
         else if (strcmp(command, "createfile") == 0) creatfile();
+        else if (strcmp(command, "cat") == 0) cat();
     }
     return 0;
 }
